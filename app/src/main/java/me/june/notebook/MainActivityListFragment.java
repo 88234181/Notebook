2496e82738541c11f91865fb28f863be7435e963
88234181 <junjiezhao722@gmail.com>
2016-06-13 19:46:36 -0700

    test

--- app/src/main/java/me/june/notebook/MainActivityListFragment.java
+++ app/src/main/java/me/june/notebook/MainActivityListFragment.java
@@ -23,7 +23,6 @@ public class MainActivityListFragment extends ListFragment {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, values);
 
         setListAdapter(adapter);
-
     }
 
     @Override
